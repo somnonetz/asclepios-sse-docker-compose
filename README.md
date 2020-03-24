433ce4ca59eb3d4155ee1ab296980387814d7678
jmz-b <jaymuhz.bee@gmail.com>
2020-03-24 20:07:27 +0100

    Intital commit
    add Dockerfiles and docker-compose definitions for SSE server, SSE client
    and TrustedAuthority services

---
+++ README.md
@@ -0,0 +1,11 @@
+# asclepios-sse-docker-compose
+
+docker-compose set up for the ASCLPIOS symmetric searchable encryption service
+
+## Usage
+
+```
+cp .env.example .env # edit this file to configure
+docker-compose build
+docker-compose up
+```
---
+++ sse-server/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+until PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USER" -h "$DB_HOST" -c '\q' "$DB_NAME"; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 5
+done
+
+>&2 echo "Postgres is up - starting service"
+
+echo "Apply database migrations"
+python3 manage.py makemigrations
+python3 manage.py migrate --run-syncdb
+
+exec gunicorn SSEServer.wsgi:application \
+    --name sse \
+    --bind 0.0.0.0:8080 \
+    --workers 3 \
+    --limit-request-line 20000 # This allows long-line requests
+    # \
+    #--log-level=info \
+    #--log-file=./logs/sse.log \
+    #--access-logfile=./logs/sse-access.log \
